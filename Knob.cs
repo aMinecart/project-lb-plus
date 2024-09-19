2fb085f18f2e8c87242977cb1ccbc0ba02991cf4
Jorbori <jorgejm091106@gmail.com>
2024-09-19 12:42:13 -0400

    Editing knobs in isolation

--- Knob.cs
+++ Knob.cs
@@ -1,9 +1,13 @@
 using System;
+using System.Collections.Generic;
 using Godot;
 
 public class Knob : Button
 {
+	[Export] private List<Button> linkList = new List<Button>();
+	
 	private bool editMode = false;
+	private bool isEditing = false;
 	private Button editToggle;
 
 	// Called when the node enters the scene tree for the first time.
@@ -12,22 +16,46 @@ public class Knob : Button
 		editToggle = GetParent().GetNode<Button>("Edit Toggle");
 	}
 
-    // Called every frame. 'delta' is the elapsed time since the previous frame.
+	// Called every frame. 'delta' is the elapsed time since the previous frame.
 	public override void _Process(float delta)
   	{
 		UpdateEditing();
+		UpdateEditStatus();
 	}
 
 	public void UpdateEditing()
 	{
 		editMode = editToggle.Pressed;
+		if(editMode)
+		{
+			this.ToggleMode = true;
+		}
+		else
+		{
+			this.Pressed = false;
+			this.ToggleMode = false;
+		}
+	}
+	
+	public void UpdateEditStatus()
+	{
+		isEditing = editMode && this.Pressed;
+		if(editMode && this.Pressed)
+		{
+			isEditing = true;
+		}
+		else
+		{
+			isEditing = false;
+		}
+		
 	}
 
 	public override void _Input(InputEvent @event)
-    {
-		if (editMode && @event is InputEventKey inputEventKey)
+	{
+		if (editMode && @event is InputEventKey inputEventKey && isEditing)
 		{
 			Text = @event.AsText();
 		}
-    }
+	}
 }
